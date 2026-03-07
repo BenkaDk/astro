892fdd44b1181d100d9b50f47bd2254406c7aa2a
BenkaDk <Privat@benka.dk>
2026-03-07 12:58:36 +0100

    Add asteroid spawning with AsteroidField, groups for updatable/drawable

--- main.py
+++ main.py
@@ -1,25 +1,51 @@
 import pygame
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 from logger import log_state
+from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 
 def main():
     print("Starting Asteroids...")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
     
-    pygame.init()
+    pygame.init() 
+    clock = pygame.time.Clock()
+    dt = 0
+
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    
+    # Player containers
+    Player.containers = (updatable, drawable)
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    
+    # Asteroid containers
+    asteroids = pygame.sprite.Group()
+    Asteroid.containers = (asteroids, updatable, drawable)
+    
+    # AsteroidField containers (kun updatable)
+    AsteroidField.containers = (updatable,)
+    asteroidfield = AsteroidField()
     
     while True:
         log_state()
+        
+        updatable.update(dt)
         
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         
         screen.fill("black")
+        for obj in drawable:
+            obj.draw(screen)
         pygame.display.flip()
-
+        
+        dt = clock.tick(60) / 1000.0
 
 if __name__ == "__main__":
     main()
